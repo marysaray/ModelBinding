d11f188c24e46c1b7205c7dbd307dc39f770212b
marysaray <82470838+marysaray@users.noreply.github.com>
2022-09-27 23:27:45 -0700

    Created a class for the object product manually.

--- ModelBinding/Controllers/ProductController.cs
+++ ModelBinding/Controllers/ProductController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ModelBinding.Models;
 
 namespace ModelBinding.Controllers
 {
     public class ProductController : Controller
     {
+        // GET: /Product/Create
+        [HttpGet]
         public IActionResult Create()
         {
+            ViewData["Title"] = "Create a new product";
             return View();
+        }
+
+        // POST: Send data to server after user submit form.
+        [HttpPost]
+        public IActionResult Create(IFormCollection formData) 
+        {
+            // Validate all data
+            
+            // Grab all the data from the IFormCollection and put in the product object
+            Product p = new()
+            {
+                Title = formData["title"],
+                Price = Convert.ToDouble(formData["price"]),
+                Qty = Convert.ToInt32(formData["qty"])
+            };
+  
+
+            // Add to database
+            // ProductDb.Add(p);
+
+            // Return data back to home page
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ ModelBinding/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace ModelBinding.Models
+{
+    /// <summary>
+    /// Represents a product for sale.
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Name of the product
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The retail price.
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// The number of units in stock.
+        /// </summary>
+        public int Qty { get; set; }
+    }
+}

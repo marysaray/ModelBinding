cc927b5a2d6e7ada2eaccef62502c3001357aea1
marysaray <82470838+marysaray@users.noreply.github.com>
2022-09-27 22:23:31 -0700

    Added controller for products.

---
+++ ModelBinding/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelBinding.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}

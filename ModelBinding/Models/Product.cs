f36be19888a74370c8fad85a20ecc02ddc44a2cc
marysaray <82470838+marysaray@users.noreply.github.com>
2022-09-28 21:27:42 -0700

    Added validation logic using DataAnnotations.

--- ModelBinding/Models/Product.cs
+++ ModelBinding/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace ModelBinding.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBinding.Models
 {
     /// <summary>
     /// Represents a product for sale.
@@ -8,16 +10,21 @@
         /// <summary>
         /// Name of the product
         /// </summary>
+        [Required(ErrorMessage = "Enter Name of Product.")]
+        [Display(Name = "Title")] // Rename label on view
         public string Title { get; set; }
 
         /// <summary>
         /// The retail price.
         /// </summary>
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be 0 or greater.")]
+        [DataType(DataType.Currency)]
         public double Price { get; set; }
 
         /// <summary>
         /// The number of units in stock.
         /// </summary>
+        [Range(0, double.MaxValue, ErrorMessage = "Quantity must be 0 or greater.")]
         public int Qty { get; set; }
     }
 }
